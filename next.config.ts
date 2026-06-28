23381e8a5291aa9e046ec03aef9f2a037e7c156f
Vinicius Saraiva <130937806+ViniciusSPDP@users.noreply.github.com>
2026-06-28 10:54:15 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -17,7 +17,11 @@ const nextConfig: NextConfig = {
     protocol: "https",
     hostname: "minio.s4r41va.com",
     pathname: "/bem-casados-bucket/**",
-  }
+  },
+            {
+        protocol: "https",
+        hostname: "placehold.co",
+      }
     ],
   },
 };
