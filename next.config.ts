2904e67a93350bfb60ec1280b37c37d7ff6222be
Vinicius Saraiva <130937806+ViniciusSPDP@users.noreply.github.com>
2026-06-28 10:52:40 -0300

    Update remotePatterns in next.config.ts
    Removed old remote patterns and added a new one for minio.s4r41va.com.

--- next.config.ts
+++ next.config.ts
@@ -13,28 +13,11 @@ const nextConfig: NextConfig = {
     contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
 
     remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "placehold.co",
-      },
-      {
-        protocol: "http",
-        hostname: "localhost",
-        port: "9002",
-        pathname: "/bem-casados-bucket/**",
-      },
-      {
-        protocol: "http",
-        hostname: "192.168.1.39", 
-        port: "9002",
-        pathname: "/bem-casados-bucket/**",
-      },
-      {
-        protocol: "https",
-        hostname: process.env.S3_ENDPOINT || "localhost",
-        port: "9002",
-        pathname: "/bem-casados-bucket/**",
-      }
+  {
+    protocol: "https",
+    hostname: "minio.s4r41va.com",
+    pathname: "/bem-casados-bucket/**",
+  }
     ],
   },
 };
